9c9a8a8747e103c2fe70d7116fda99fd00c6787e
한상운 <how1994@naver.com>
2023-02-19 20:00:05 +0900

    임무 배치 UI 중 작전 종료 경고창 추가, 경고창 UI 프리팹화, 작전 상단 UI 프리팹화

--- Assets/Scripts/UIs/GaugeDisplayText.cs
+++ Assets/Scripts/UIs/GaugeDisplayText.cs
@@ -10,24 +10,6 @@ public class GaugeDisplayText : MonoBehaviour
     public TextMeshProUGUI text;
     public Slider slider;
 
-    private void Start()
-    {
-        slider.value = slider.maxValue;
-        UpdateText();
-    }
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.V))
-        {
-            slider.value += 5f;
-        }
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            slider.value -= 5f;
-        }
-    }
-
     public void UpdateText()
     {
         text.text = $"{slider.value:.} / {slider.maxValue:.}";
