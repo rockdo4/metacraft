92224eca82af091e2e5d0573f7fdb8cfdb0a9352
서진석 <ting6247@nate.com>
2023-02-20 00:44:46 +0900

    정렬 기능 추가

--- Assets/Scripts/Character/CharacterData.cs
+++ Assets/Scripts/Character/CharacterData.cs
@@ -2,15 +2,23 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class CharacterData
+public class CharacterData : IComparable<CharacterData>
 {
     public string heroName;
     public string grade;
     public string type;
     public string level;
 
+    //public Sprite sprite;
+
+    public int CompareTo(CharacterData other)
+    {
+        return heroName.CompareTo(other.heroName);
+    }
+
     public void PrintState()
     {
         Logger.Debug($"{heroName} {grade} {type} {level}");
+        //sprite = Resources.Load<Sprite>(iconSpriteId);
     }
 }
