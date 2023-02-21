3dcdf74f60d7ee5980e84e426a9ef33aec2511d5
함민우 <108320084+minu1117@users.noreply.github.com>
2023-02-21 22:56:28 +0900

    BattleManager 맵 이동 완성 - 맵에 설치된 Trigger의 위치를 비교하여 이동, Trigger 내부의 Pos2의 위치에 도달하면 정지. - 테스트 코드로 TempMoveTest() 함수와 Update 함수에 A 버튼으로 실행할 수 있는 코드 추가.
    Trigger 오브젝트 Prefab화.
    - 설정해야 하는 항목들은 Header를 사용하여 인스펙터창에 설명 추가.

--- Assets/Scripts/Battle/TestBattleManager.cs
+++ Assets/Scripts/Battle/TestBattleManager.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestBattleManager : MonoBehaviour
 {
-    public AttackableHero cube;
+    public GameObject platform;
+    public float platformMoveSpeed;
 
+    public AttackableHero cube;
     public BattleHero battleHero;
     public Transform battleHeroTr;
 
     public List<Transform> startPositions;
+    public List<EventTrigger> triggers;
+    private int currTriggerIndex = 0;
+    private int readyCount = 3;
+    private float nextStageMoveTimer = 0f;
 
     private void Awake()
     {
@@ -22,6 +30,67 @@ public class TestBattleManager : MonoBehaviour
             var heroUi = Instantiate(battleHero, battleHeroTr);
 
             hero.SetUi(heroUi);
+        }
+    }
+
+    public List<Transform> GetStartPosition()
+    {
+        return startPositions;
+    }
+
+    public void Ready()
+    {
+        readyCount--;
+        for (int i = 0; i < triggers.Count; i++)
+        {
+            if (readyCount == 0 && !triggers[i].isStageEnd)
+            {
+                readyCount = 3;
+                StartCoroutine(MovingMap());
+                return;
+            }
+        }
+    }
+    
+    private void TempMoveTest()
+    {
+        for (int i = 0; i < triggers.Count; i++)
+        {
+            if (!triggers[i].isStageEnd)
+            {
+                StartCoroutine(MovingMap());
+                return;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TempMoveTest();
+        }
+    }
+
+    // 클리어 시 호출할 함수 (적어둘 내용들 몰라서 아직 안 적음)
+    public void StageClear()
+    {
+
+    }
+
+    IEnumerator MovingMap()
+    {
+        yield return new WaitForSeconds(nextStageMoveTimer);
+
+        var curPos = platform.gameObject.transform.position.z + triggers[currTriggerIndex].settingPosition.position.z;
+        var nextPos = triggers[currTriggerIndex + 1].settingPosition.position.z;
+        var movePos = curPos - nextPos;
+
+        currTriggerIndex++;
+
+        while (platform.gameObject.transform.position.z >= movePos)
+        {
+            platform.gameObject.transform.Translate((Vector3.forward * platformMoveSpeed * Time.deltaTime) * -1);
+            yield return new WaitForSeconds(Time.deltaTime);
         }
     }
 }
