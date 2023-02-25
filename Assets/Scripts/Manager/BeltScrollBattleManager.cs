302c1211d6f8c75bb22d6e8b93c9ff6cf01a6bf2
함민우 <108320084+minu1117@users.noreply.github.com>
2023-02-25 19:07:22 +0900

    MapEventTrigger 스크립트, BeltScrollBattleManager 스크립트 안 쓰는 using 제거

--- Assets/Scripts/Manager/BeltScrollBattleManager.cs
+++ Assets/Scripts/Manager/BeltScrollBattleManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class BeltScrollBattleManager : TestBattleManager
 {
