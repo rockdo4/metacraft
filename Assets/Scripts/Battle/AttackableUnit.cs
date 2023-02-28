193760d51ba496c17819ad271b6a246881a80a85
서진석 <ting6247@nate.com>
2023-03-01 00:20:42 +0900

    배틀씬 적 유닛 사이즈 이슈 수정

--- Assets/Scripts/Battle/AttackableUnit.cs
+++ Assets/Scripts/Battle/AttackableUnit.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UIElements;
 
 public abstract class AttackableUnit : MonoBehaviour
 {
