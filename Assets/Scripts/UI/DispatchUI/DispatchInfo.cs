885cb96d1b20ff02f65976bb34c01621b68af4e6
한상운 <how1994@naver.com>
2023-03-03 00:36:51 +0900

    파견 데이터 연동 수정 및 버튼 연결

--- Assets/Scripts/UI/DispatchUI/DispatchInfo.cs
+++ Assets/Scripts/UI/DispatchUI/DispatchInfo.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class DispatchInfo : MonoBehaviour
 {
     //before
+    public GameObject before;
     public Image addRewardCharacterPortrait;
     public TextMeshProUGUI dispatchDescription;
     public TextMeshProUGUI beforeButton;
     //proceeding
+    public GameObject proceeding;
     public Image turnPortrait;
     public TextMeshProUGUI turn;
     public Image[] heroPortarits;
@@ -22,6 +24,18 @@ public class DispatchInfo : MonoBehaviour
         addRewardCharacterPortrait.sprite = GameManager.Instance.iconSprites[$"Icon_{dispatchInfo["AddRewardCharaID"]}"];
         dispatchDescription.text = $"{dispatchInfo["Time"]}\n{dispatchInfo["Name"]}\n{dispatchInfo["Text"]}";
         beforeButton.text = $"ÆÄ°ß";
+    }
+
+    public void OnClickBeforeButton()
+    {
+        before.SetActive(false);
+        proceeding.SetActive(true);
+    }
+
+    public void OnClickProceedingButton()
+    {
+        proceeding.SetActive(false);
+        before.SetActive(true);
     }
 
     //public override void OnClick()
