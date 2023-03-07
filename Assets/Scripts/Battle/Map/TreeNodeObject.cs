37af5d27cefac392055fa197b69030b338e6bef3
서진석 <ting6247@nate.com>
2023-03-07 18:32:12 +0900

    노드 연결선 출력 성공

--- Assets/Scripts/Battle/Map/TreeNodeObject.cs
+++ Assets/Scripts/Battle/Map/TreeNodeObject.cs
@@ -1,35 +1,32 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public struct TreeNodeObject<T>
+public class TreeNodeObject : MonoBehaviour, IComparable<TreeNodeObject>
 {
-    public T data;
-    public TreeNodeTypes type;
-    public List<TreeNodeObject<T>> childrens;
-
-    public TreeNodeObject(T data, TreeNodeTypes type = TreeNodeTypes.None)
-    {
-        this.data = data;
-        this.type = type;
-        childrens = new ();
-    }
-
-    public void AddChildren(T data, TreeNodeTypes type)
-    {
-        AddChildren(new TreeNodeObject<T>(data, type));
-    }
-
-    public void AddChildren(TreeNodeObject<T> node)
-    {
-        childrens.Add(node);
-    }
-}
-
-public class TreeNodeObject : MonoBehaviour
-{
-    public TreeNodeObject<string> treeNode;
     public TextMeshProUGUI nodeNameText;
     public Transform head;
     public Transform tail;
+
+    public string data;
+    public TreeNodeTypes type;
+    public List<TreeNodeObject> childrens = new ();
+
+    public void SetInit(string data, TreeNodeTypes type = TreeNodeTypes.None)
+    {
+        this.data = data;
+        this.type = type;
+        nodeNameText.text = data;
+    }
+
+    public void AddChildren(TreeNodeObject node)
+    {
+        childrens.Add(node);
+    }
+
+    public int CompareTo(TreeNodeObject other)
+    {
+        return data.CompareTo(other.data);
+    }
 }
