8efb05f3f3e6f3ec2f7f82e4e7f96def1b8cd7a9
이정연 <lofl.lotus.flower@gmail.com>
2023-03-08 15:22:54 +0900

    스킬 이펙트 틀 작업

--- Assets/Scripts/Effect/SkillEffect.cs
+++ Assets/Scripts/Effect/SkillEffect.cs
@@ -1,16 +1,17 @@
-using System;
 using UnityEngine;
 
 public class SkillEffect : Effect
 {
-    public Action OnDamage;
+    public LiveData data;
+    public CharacterSkill skill;
     public string targetTag;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag(targetTag))
-        { 
+        {
+            int dmg = skill.CreateDamageResult(data);
+            other.GetComponent<AttackableUnit>().OnDamage(dmg);
         }
     }
-
 }
