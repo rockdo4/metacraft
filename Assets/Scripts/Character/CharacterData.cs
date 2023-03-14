72ec473980cab2c4277366f3a20d72e711ddfee3
서진석 <ting6247@nate.com>
2023-03-14 10:15:53 +0900

    데이터클래스 정리

--- Assets/Scripts/Character/CharacterData.cs
+++ Assets/Scripts/Character/CharacterData.cs
@@ -8,7 +8,7 @@ public class CharacterData : ScriptableObject, IComparable<CharacterData>
     [Range(1, 5)]
     public int          grade = 1;          // 등급
     [Range(1, 5)]
-    public int          maxGrade = 1;       // 승급 가능 최대 등급
+    public int          maxGrade = 5;       // 승급 가능 최대 등급
 
     public string       job;                // 직업, 특성
 
@@ -16,7 +16,7 @@ public class CharacterData : ScriptableObject, IComparable<CharacterData>
     public int          level = 1;          // 레벨
     public int          exp = 0;            // 경험치
     [Range(0, 1000)]
-    public int          likeability;        // 호감도
+    public int          likeability = 0;    // 호감도
 
     //public int Power
     //{
@@ -25,20 +25,20 @@ public class CharacterData : ScriptableObject, IComparable<CharacterData>
     //    get { return power; }
     //}
 
-    //private int power;              // 전투력
+    //private int power;            // 전투력
     public int baseDamage = 50;     // 일반 공격 데미지
     public int baseDefense = 0;     // 방어력
     public int healthPoint = 500;   // 최대 체력
     public int currentHp = 500;     // 현재 체력
-    public int moveSpeed = 3;        // 이동 속도. 범위, 초기값 설정 필요
+    public int moveSpeed = 3;       // 이동 속도. 범위, 초기값 설정 필요
     [Range(0f, 1f)]
-    public float critical = 0f;      // 크리티컬 확률
+    public float critical = 0f;     // 크리티컬 확률
     [Range(1f, 10f)]
-    public float criticalDmg = 2f;   // 크리티컬 데미지 배율
+    public float criticalDmg = 2f;  // 크리티컬 데미지 배율
     [Range(0f, 1f)]
-    public float evasion = 0f;       // 회피율
+    public float evasion = 0f;      // 회피율
     [Range(0f, 1f)]
-    public float accuracy = 1f;    // 명중률
+    public float accuracy = 1f;     // 명중률
 
     public int CompareTo(CharacterData other)
     {
