56a56f7a71afabcbd7aaa6c5cbfdc386cb7f284f
한상운 <how1994@naver.com>
2023-03-15 15:55:57 +0900

    캐릭터데이터 스크립트 데이터테이블 순서 매칭

--- Assets/Scripts/Character/CharacterData.cs
+++ Assets/Scripts/Character/CharacterData.cs
@@ -11,30 +11,31 @@ public class CharacterData : ScriptableObject, IComparable<CharacterData>
     public int          grade = 1;          // 등급
     [Range(1, 5)]
     public int          maxGrade = 5;       // 승급 가능 최대 등급
-
     public int          job;                // 직업
+
+    // 스텟
+    public int          baseDamage = 50;    // 일반 공격 데미지
+    public int          baseDefense = 0;    // 방어력
+    public int          healthPoint = 500;  // 최대 체력
+    [Range(0f, 1f)]
+    public float        critical = 0f;      // 크리티컬 확률
+    [Range(1f, 10f)]
+    public float        criticalDmg = 2f;   // 크리티컬 데미지 배율
+    public int          moveSpeed = 3;      // 이동 속도. 범위, 초기값 설정 필요
+    [Range(0f, 1f)]
+    public float        accuracy = 1f;      // 명중률
+    [Range(0f, 1f)]
+    public float        evasion = 0f;       // 회피율
     public List<string> tags;               // 태그(특성)
 
-    [Range(1, 20)]
+    //LiveData
+    public int          currentHp = 500;    // 현재 체력
+    [Range(1, 50)]
     public int          level = 1;          // 레벨
     public int          exp = 0;            // 경험치
     [Range(0, 1000)]
     public int          likeability = 0;    // 호감도
 
-    // 스텟
-    public int baseDamage = 50;     // 일반 공격 데미지
-    public int baseDefense = 0;     // 방어력
-    public int healthPoint = 500;   // 최대 체력
-    public int currentHp = 500;     // 현재 체력
-    public int moveSpeed = 3;       // 이동 속도. 범위, 초기값 설정 필요
-    [Range(0f, 1f)]
-    public float critical = 0f;     // 크리티컬 확률
-    [Range(1f, 10f)]
-    public float criticalDmg = 2f;  // 크리티컬 데미지 배율
-    [Range(0f, 1f)]
-    public float evasion = 0f;      // 회피율
-    [Range(0f, 1f)]
-    public float accuracy = 1f;     // 명중률
 
     public int CompareTo(CharacterData other)
     {
