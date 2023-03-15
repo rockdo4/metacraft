c51fb82963a6b2446edc1d0e8a2211f6c645635b
김영우 <oos1928@gmail.com>
2023-03-15 12:15:10 +0900

    게임매니저 이펙트 등록

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
