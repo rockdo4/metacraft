1f140de2f89253642ebf292206b2a0fbb1dece33
김영우 <oos1928@gmail.com>
2023-03-16 21:28:17 +0900

    수아평타 안맞는 버그 수정 // 테이블 최대타겟이 -1로 돼있었음

--- Assets/Hovl Studio/AAA Projectiles Vol 2/Scripts/ProjectileMover.cs
+++ Assets/Hovl Studio/AAA Projectiles Vol 2/Scripts/ProjectileMover.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class ProjectileMover : MonoBehaviour
 {
@@ -7,22 +8,26 @@ public class ProjectileMover : MonoBehaviour
     public bool UseFirePointRotation;
     public Vector3 rotationOffset = new Vector3(0, 0, 0);
     public GameObject hit;
+    private GameObject hitInstaiated;
     public GameObject flash;
     private Rigidbody rb;
     public GameObject[] Detached;
 
-    private ParticleSystem ps;
+    private string activeOffFuncName;
+
+    //private ParticleSystem ps;
 
     private void Awake()
     {
-        ps = GetComponent<ParticleSystem>();
+        //ps = GetComponent<ParticleSystem>();
         rb = GetComponent<Rigidbody>();
+        activeOffFuncName = nameof(SetActiveOffGameObject);
     }
 
     void Start()
     {
-        hit = Instantiate(hit);
-        hit.SetActive(false);
+        hitInstaiated = Instantiate(hit);
+        hitInstaiated.SetActive(false);
         //if (flash != null)
         //{
         //    var flashInstance = Instantiate(flash, transform.position, Quaternion.identity);
@@ -42,7 +47,8 @@ public class ProjectileMover : MonoBehaviour
     }
     private void OnEnable()
     {
-        rb.velocity = transform.parent.forward * speed;        
+        rb.velocity = transform.parent.forward * speed;
+        Invoke(activeOffFuncName, 0.5f);
     }
 
     private void OnDisable()
@@ -51,57 +57,62 @@ public class ProjectileMover : MonoBehaviour
         rb.angularVelocity = Vector3.zero;
     }
 
- //   void FixedUpdate ()
- //   {
-	//	if (speed != 0)
- //       {
- //           //rb.velocity = transform.forward * speed;
- //           //transform.position += transform.forward * (speed * Time.deltaTime);         
- //       }
-	//}
+    private void OnTriggerEnter(Collider other)
+    {
+        //hitInstaiated.transform.position = other.ClosestPoint();
+        //hitInstaiated.SetActive(true);
+        //hitInstaiated.GetComponent<ParticleSystem>().Play();
+
+        gameObject.SetActive(false);
+    }
+
+    private void SetActiveOffGameObject()
+    {
+        gameObject.SetActive(false);
+    }
 
     //https ://docs.unity3d.com/ScriptReference/Rigidbody.OnCollisionEnter.html
- 
-    void OnCollisionEnter(Collision collision)
-    {
-        //Lock all axes movement and rotation
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
-        //speed = 0;
 
-        ContactPoint contact = collision.contacts[0];
-        //Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 pos = contact.point + contact.normal * hitOffset;
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    Lock all axes movement and rotation
+    //    rb.constraints = RigidbodyConstraints.FreezeAll;
+    //    speed = 0;
 
-       // if (hit != null)
-      //  {
-            //var hitInstance = Instantiate(hit, pos, rot);
-            //if (UseFirePointRotation) { hitInstance.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 180f, 0); }
-            //else if (rotationOffset != Vector3.zero) { hitInstance.transform.rotation = Quaternion.Euler(rotationOffset); }
-            //else { hitInstance.transform.LookAt(contact.point + contact.normal); }
+    //    ContactPoint contact = collision.contacts[0];
+    //    Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+    //    Vector3 pos = contact.point + contact.normal * hitOffset;
 
-            //var hitPs = hitInstance.GetComponent<ParticleSystem>();
-            //if (hitPs != null)
-            //{
-            //    //Destroy(hitInstance, hitPs.main.duration);
-            //}
-            //else
-            //{
-            //    //var hitPsParts = hitInstance.transform.GetChild(0).GetComponent<ParticleSystem>();
-            //    //Destroy(hitInstance, hitPsParts.main.duration);
-            //}
-        //}
-        hit.transform.position = pos;
-        hit.SetActive(true);
-        hit.GetComponent<ParticleSystem>().Play();
-        //foreach (var detachedPrefab in Detached)
-        //{
-        //    if (detachedPrefab != null)
-        //    {
-        //        detachedPrefab.transform.parent = null;
-        //    }
-        //}
-        //Destroy(gameObject);        
-        gameObject.SetActive(false);
-        //ps.Stop();
-    }
+    //    if (hit != null)
+    //    {
+    //        var hitInstance = Instantiate(hit, pos, rot);
+    //        if (UseFirePointRotation) { hitInstance.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 180f, 0); }
+    //        else if (rotationOffset != Vector3.zero) { hitInstance.transform.rotation = Quaternion.Euler(rotationOffset); }
+    //        else { hitInstance.transform.LookAt(contact.point + contact.normal); }
+
+    //        var hitPs = hitInstance.GetComponent<ParticleSystem>();
+    //        if (hitPs != null)
+    //        {
+    //            //Destroy(hitInstance, hitPs.main.duration);
+    //        }
+    //        else
+    //        {
+    //            //var hitPsParts = hitInstance.transform.GetChild(0).GetComponent<ParticleSystem>();
+    //            //Destroy(hitInstance, hitPsParts.main.duration);
+    //        }
+    //    }
+    //    hitInstaiated.transform.position = pos;
+    //    hitInstaiated.SetActive(true);
+    //    hitInstaiated.GetComponent<ParticleSystem>().Play();
+    //    foreach (var detachedPrefab in Detached)
+    //    {
+    //        if (detachedPrefab != null)
+    //        {
+    //            detachedPrefab.transform.parent = null;
+    //        }
+    //    }
+    //    Destroy(gameObject);
+    //    gameObject.SetActive(false);
+    //    ps.Stop();
+    //}
 }
