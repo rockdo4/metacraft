3eb4811556262317556a885e91c0672c45154068
이정연 <lofl.lotus.flower@gmail.com>
2023-03-24 12:44:43 +0900

    인벤토리에 함수들 추가, id로 아이템 추가 가능하게 변경, 스트링 테이블 업데이트

--- Assets/Scripts/Manager/InventoryData.cs
+++ Assets/Scripts/Manager/InventoryData.cs
@@ -7,14 +7,22 @@ public class InventoryData
     public int count;
     public List<Item> inventory = new();
 
-    public void UseItem(Item item, int count)
+    public void UseItem(string id, int count)
     {
-        var idx = FindItem(item);
+        var idx = FindItemIndexCheckCount(id, count);
         if (idx == -1)
             return;
         else
-            inventory[idx].DelCount(count);
+        {
+            if (inventory[idx].count == count)
+            {
+                inventory.RemoveAt(idx);
+            }
+            else
+                inventory[idx].DelCount(count);
+        }
     }
+
     public void AddItem(Item item)
     {
         if (item.id.CompareTo("60300001") == 0)
@@ -24,22 +32,76 @@ public class InventoryData
                 return;
             }
         }
-        var idx = FindItem(item);
+        var idx = FindItemIndex(item.id);
         if (idx == -1)
             inventory.Add(item);
         else
             inventory[idx].AddCount(item.count);
     }
 
-    public int FindItem(Item item)
+    public void AddItem(string id, int count = 1)
     {
-        InventoryData inventoryData = GameManager.Instance.inventoryData;
-        for (int i = 0; i < inventoryData.inventory.Count; i++)
+        if (id.CompareTo("60300001") == 0)
         {
-            if (inventoryData.inventory[i].id == item.id)
+            GameManager.Instance.playerData.gold += count;
+            {
+                return;
+            }
+        }
+        var idx = FindItemIndex(id);
+        if (idx == -1)
+        {
+            var data = GameManager.Instance.itemInfoList.Find(t => t["ID"].ToString().CompareTo(id) == 0);
+            Item newItem = new Item(data);
+            newItem.count = count;
+            inventory.Add(newItem);
+        }
+        else
+            inventory[idx].AddCount(count);
+    }
+
+    // 아이템 인덱스 찾기
+    public int FindItemIndex(string id)
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (inventory[i].id == id)
                 return i;
         }
         return -1;
+    }
+
+    // 아이템 인덱스 찾고 원하는 개수만큼 있는지 확인
+    public int FindItemIndexCheckCount(string id, int count)
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (inventory[i].id == id)
+            {
+                if (inventory[i].count < count)
+                    return -1;
+                else
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    // 아이템이 있는지 확인
+    public bool IsItem(string id)
+    {
+        return inventory[FindItemIndex(id)] != null;
+    }
+    //특정 개수만큼 아이템이 있는지 확인
+    public bool IsItem(string id, int count)
+    {
+        var item = inventory[FindItemIndex(id)];
+        return (item != null) && (item.count >= count);
+    }
+    //아이디로만 아이템 가져오기. 없으면 null
+    public Item GetItem(string id)
+    {
+        return inventory[FindItemIndex(id)];
     }
 }
 
@@ -54,6 +116,27 @@ public class Item
     public string sort;
     public string dataID;
 
+    public Item() { }
+    public Item(string id, string name, int count, string info, string iconName, string sort, string dataID)
+    {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.info = info;
+        this.iconName = iconName;
+        this.sort = sort;
+        this.dataID = dataID;
+    }
+    public Item(Dictionary<string, object> data)
+    {
+        this.id = data["ID"].ToString();
+        this.name = data["Item_Name"].ToString();
+        this.count = 0;
+        this.info = data["Info"].ToString();
+        this.iconName = data["Icon_Name"].ToString();
+        this.sort = data["Sort"].ToString();
+        this.dataID = data["DataID"].ToString();
+    }
     public void AddCount(int cnt)
     {
         count += cnt;
