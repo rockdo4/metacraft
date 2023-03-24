409ef91448b8ef848366ae116dfd42925934e840
한상운 <how1994@naver.com>
2023-03-24 12:31:58 +0900

    승급 기능 시퀀스 완료 (재료부분  X)

--- Assets/Scripts/UI/UpgradeUI/HeroUpgradeDetailWindow.cs
+++ Assets/Scripts/UI/UpgradeUI/HeroUpgradeDetailWindow.cs
@@ -35,7 +35,7 @@ public class HeroUpgradeDetailWindow : View
     private void OnEnable()
     {
         SetHeroInfo();
-    }
+    }   
 
     private void SetHeroInfo()
     {
@@ -45,50 +45,78 @@ public class HeroUpgradeDetailWindow : View
         LiveData data = cdb.data;
 
         heroName.text = gm.GetStringByTable(data.name);
-        heroGrade.text = $"{(CharacterGrade)data.grade}등급 > {(CharacterGrade)(data.grade + 1)}등급";
-        heroMaxLevel.text = $"{gm.maxLevelTable[data.grade]["MaxLevel"]} > {gm.maxLevelTable[data.grade + 1]["MaxLevel"]}";
+        CharacterGrade curGrade = (CharacterGrade)data.grade;
+        if (curGrade != CharacterGrade.SS)
+        {
+            heroGrade.text = $"{curGrade}등급 > {curGrade + 1}등급"; 
+            heroMaxLevel.text =
+                $"{gm.maxLevelTable[data.grade - 1]["MaxLevel"]} >" +
+                $"{gm.maxLevelTable[data.grade]["MaxLevel"]}";
+
+            resultHeroIcon.sprite = gm.GetSpriteByAddress($"icon_{data.name}");
+            resultHeroText.text = $"승급 심사 통과를 축하합니다!\n{(CharacterGrade)(data.grade + 1)}등급 히어로로 승급하였습니다!";
+        }
+        else
+        {
+            heroGrade.text = $"{curGrade}등급";
+            heroMaxLevel.text =
+                $"{gm.maxLevelTable[data.grade - 1]["MaxLevel"]}";
+
+            resultHeroIcon.sprite = gm.GetSpriteByAddress($"icon_{data.name}");
+            resultHeroText.text = $"이미 {(CharacterGrade)(data.grade)}등급입니다";
+        }
+
         heroPortrait.sprite = gm.GetSpriteByAddress($"icon_{data.name}");
-        //prevSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
+        prevSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
         prevSkillIcons[1].sprite = gm.GetSpriteByAddress($"{cdb.attacks[0].skillIcon}");
         prevSkillIcons[2].sprite = gm.GetSpriteByAddress($"{cdb.activeSkill.skillIcon}");
-        //prevSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
+        prevSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
         prevSkillInfo[1].text = gm.GetStringByTable($"{cdb.attacks[0].skillDescription}");
         prevSkillInfo[2].text = gm.GetStringByTable($"{cdb.activeSkill.skillDescription}");
 
-        //nextSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
+        nextSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
         nextSkillIcons[1].sprite = gm.GetSpriteByAddress($"{cdb.attacks[0].skillIcon}");
         nextSkillIcons[2].sprite = gm.GetSpriteByAddress($"{cdb.activeSkill.skillIcon}");
-        //nextSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
+        nextSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
         nextSkillInfo[1].text = gm.GetStringByTable($"{cdb.attacks[0].skillDescription}");
         nextSkillInfo[2].text = gm.GetStringByTable($"{cdb.activeSkill.skillDescription}");
 
         FindUpgradeMaterial(data.name, data.grade);
-
-        resultHeroIcon.sprite = gm.GetSpriteByAddress($"icon_{data.name}");
-        resultHeroText.text = $"승급 심사 통과를 축하합니다!\n{(CharacterGrade)(data.grade + 1)}등급 히어로로 승급하였습니다!";
-
     }
 
     private void FindUpgradeMaterial(string name, int grade)
     {
         for (int i = 0; i < gm.upgradeTable.Count; i++)
         {
-            if (gm.upgradeTable[i]["Name"].ToString() == name&&(int)gm.upgradeTable[i]["Grade"] == grade)
+            var itemName = gm.upgradeTable[i]["Name"].ToString();
+            if (itemName == name && (int)gm.upgradeTable[i]["Grade"] == grade)
             {
-                meterialIcons[0].sprite = gm.GetSpriteByAddress(gm.inventoryData.FindItem((string)gm.upgradeTable[i]["Material1"]).iconName);
-                meterialInfo[0].text = $"{0}/{gm.upgradeTable[i]["Mmaterial1Amount"]}";
-                gold.text = $"{gm.playerData.gold}/{gm.upgradeTable[i]["NeedGold"]}";
+                foreach (var item in gm.itemInfoList)
+                {
+                    if (item["ID"].Equals(gm.upgradeTable[i]["Material1"].ToString()))
+                    {
+                        meterialIcons[0].sprite = gm.GetSpriteByAddress(item["Icon_Name"].ToString());
+                    }
+                }
             }
+            Item thisItem = gm.inventoryData.FindItem(itemName);
+            var itemCount = thisItem == null ? 0 : gm.inventoryData.FindItem(itemName).count;
+            meterialInfo[0].text = $"{itemCount}/{gm.upgradeTable[i]["Mmaterial1Amount"]}";
+            
+            gold.text = $"{gm.playerData.gold}/{gm.upgradeTable[i]["NeedGold"]}";
         }
     }
+
 
     public void OnClickUpgradeButton()
     {
         CharacterDataBundle cdb = gm.currentSelectObject.GetComponent<AttackableUnit>().GetUnitData();
         LiveData data = cdb.data;
+        if (data.grade == (int)CharacterGrade.SS)
+            return;
 
+        data.maxLevel = (int)gm.maxLevelTable[data.grade]["MaxLevel"];
         data.grade += 1;
-        data.maxLevel = (int)gm.maxLevelTable[data.grade + 1]["MaxLevel"];
         cdb.attacks[0].skillLevel += 1;
         cdb.passiveSkill.skillLevel += 1;
         cdb.activeSkill.skillLevel += 1;
