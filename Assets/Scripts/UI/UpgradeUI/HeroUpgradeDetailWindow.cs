4e66640cd353dbf6bc5a6cc455f96562ea842c78
한상운 <how1994@naver.com>
2023-03-24 10:48:56 +0900

    캐릭터 스킬 데이터 스킬 아이콘 정보 추가

--- Assets/Scripts/UI/UpgradeUI/HeroUpgradeDetailWindow.cs
+++ Assets/Scripts/UI/UpgradeUI/HeroUpgradeDetailWindow.cs
@@ -27,7 +27,7 @@ public class HeroUpgradeDetailWindow : View
 
     private GameManager gm;
 
-    private void Start()
+    private void Awake()
     {
         gm = GameManager.Instance;
     }
@@ -48,17 +48,17 @@ public class HeroUpgradeDetailWindow : View
         heroGrade.text = $"{(CharacterGrade)data.grade}등급 > {(CharacterGrade)(data.grade + 1)}등급";
         heroMaxLevel.text = $"{gm.maxLevelTable[data.grade]["MaxLevel"]} > {gm.maxLevelTable[data.grade + 1]["MaxLevel"]}";
         heroPortrait.sprite = gm.GetSpriteByAddress($"icon_{data.name}");
-        prevSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
+        //prevSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
         prevSkillIcons[1].sprite = gm.GetSpriteByAddress($"{cdb.attacks[0].skillIcon}");
         prevSkillIcons[2].sprite = gm.GetSpriteByAddress($"{cdb.activeSkill.skillIcon}");
-        prevSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
+        //prevSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
         prevSkillInfo[1].text = gm.GetStringByTable($"{cdb.attacks[0].skillDescription}");
         prevSkillInfo[2].text = gm.GetStringByTable($"{cdb.activeSkill.skillDescription}");
 
-        nextSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
+        //nextSkillIcons[0].sprite = gm.GetSpriteByAddress($"{cdb.passiveSkill.skillIcon}");
         nextSkillIcons[1].sprite = gm.GetSpriteByAddress($"{cdb.attacks[0].skillIcon}");
         nextSkillIcons[2].sprite = gm.GetSpriteByAddress($"{cdb.activeSkill.skillIcon}");
-        nextSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
+        //nextSkillInfo[0].text = gm.GetStringByTable($"{cdb.passiveSkill.skillDescription}");
         nextSkillInfo[1].text = gm.GetStringByTable($"{cdb.attacks[0].skillDescription}");
         nextSkillInfo[2].text = gm.GetStringByTable($"{cdb.activeSkill.skillDescription}");
 
@@ -75,7 +75,7 @@ public class HeroUpgradeDetailWindow : View
         {
             if (gm.upgradeTable[i]["Name"].ToString() == name&&(int)gm.upgradeTable[i]["Grade"] == grade)
             {
-                //meterialIcons[0] = gm.inventoryData.FindItem(gm.upgradeTable[i]["Material1"])
+                meterialIcons[0].sprite = gm.GetSpriteByAddress(gm.inventoryData.FindItem((string)gm.upgradeTable[i]["Material1"]).iconName);
                 meterialInfo[0].text = $"{0}/{gm.upgradeTable[i]["Mmaterial1Amount"]}";
                 gold.text = $"{gm.playerData.gold}/{gm.upgradeTable[i]["NeedGold"]}";
             }
