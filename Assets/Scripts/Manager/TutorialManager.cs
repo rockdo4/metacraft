3418e95993cae7b1bc4c97d2690700c685d58bde
함민우 <108320084+minu1117@users.noreply.github.com>
2023-03-27 16:32:10 +0900

    튜토리얼 관련 스크립트들 작성중

--- Assets/Scripts/Manager/TutorialManager.cs
+++ Assets/Scripts/Manager/TutorialManager.cs
@@ -1,58 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    private Color outlineColor;
-    private Color outlineSettingColor;
-    //public List<GameObject> chatWindows;
-    //private List<TextMeshProUGUI> texts = new();
+    public List<TutorialButton> tutorialButtonList = new();
     private int currChatWindowIndex = 0;
     private int chatLine = 0;
-
-    private void Start()
-    {
-        //for (int i = 0; i < chatWindows.Count; i++)
-        //{
-        //    texts.Add(chatWindows[i].GetComponentInChildren<TextMeshProUGUI>());
-        //}
-    }
-
-    public void OnOutline(Outline outlinePrefab)
-    {
-        outlineColor = outlinePrefab.OutlineColor;
-        outlinePrefab.OutlineColor = outlineSettingColor;
-    }
-
-    public void OffOutline(Outline outlinePrefab)
-    {
-        outlinePrefab.OutlineColor = outlineColor;
-    }
-
-    public void SetOutlineColor(Color color)
-    {
-        outlineSettingColor = color;
-    }
 
     public void OnChatWindow(int index)
     {
         currChatWindowIndex = index;
-        //chatWindows[index].SetActive(true);
+        tutorialButtonList[index].OnWindow();
     }
 
     public void OffChatWindow()
     {
-        //chatWindows[currChatWindowIndex].SetActive(false);
+        tutorialButtonList[currChatWindowIndex].OffWindow();
     }
 
     public void OnNextChatLine()
     {
         // 여기서 채팅 줄 관리
-        //texts[currChatWindowIndex].text = ~
+        //tutorialButtonList[currChatWindowIndex].SetText(string);
     }
 
     public void OnSkip()
     {
-        // 대사 줄 이동
+        // 대사 줄 스킵 구간까지 이동
         // 다음에 출력하는 UI들 인덱스 설정
     }
 }
