958c9e6ba7a89901500dbfd24bbfd96f1901d79b
김영우 <oos1928@gmail.com>
2023-04-06 14:23:46 +0900

    튜토리얼용 상호작용 가능 패널 완성

---
+++ Assets/Scripts/Tutorial/TutorialNoneBlockPanels.cs
@@ -0,0 +1,19 @@
+public class TutorialNoneBlockPanels : TutorialBlockPanels
+{
+    public static new TutorialNoneBlockPanels Instance;
+
+    public TutorialManager tutorialManager;
+    public override void Awake()
+    {
+        Instance = this;
+
+        bottom.color = panelColor;
+        left.color = panelColor;
+        top.color = panelColor;
+        right.color = panelColor;
+    }
+    public void OnNextTutorialEvent()
+    {
+        tutorialManager.OnNextTutorialEvent();        
+    }
+}
