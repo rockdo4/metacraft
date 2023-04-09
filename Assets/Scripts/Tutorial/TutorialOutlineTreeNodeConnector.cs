c13deb634ee3aa1829e6427a21c6b311cb742f4b
김영우 <oos1928@gmail.com>
2023-04-09 14:20:54 +0900

    노드 / 아웃라인 위치 수정 완료 / 아웃인 이벤트 연결 완료

---
+++ Assets/Scripts/Tutorial/TutorialOutlineTreeNodeConnector.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TutorialOutlineTreeNodeConnector : MonoBehaviour
+{
+    private TutorialOutline tutorialOutline;
+    public GameObject nodes;
+
+    public int bundleIndex;
+    public int nodeIndex;
+
+    private void Awake()
+    {
+        tutorialOutline = GetComponent<TutorialOutline>();
+        ConnectOutlineToTreeNode();
+    }
+
+    private void ConnectOutlineToTreeNode()
+    {
+        var bundle = nodes.transform.GetChild(bundleIndex);
+        var node = bundle.GetChild(nodeIndex);
+
+        tutorialOutline.originalButton = node.gameObject;
+    }
+}
