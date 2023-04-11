e4243630a7d57b262f2a7d845ac684fbdea7bb0b
한상운 <how1994@naver.com>
2023-04-11 18:30:52 +0900

    인벤토리 UI 변경

--- Assets/Scripts/UI/Inventory/InventoryTab.cs
+++ Assets/Scripts/UI/Inventory/InventoryTab.cs
@@ -7,31 +7,34 @@ public class InventoryTab : MonoBehaviour
     public Button[] buttons;
     public Button nowButton;
 
+    private Color select = new(0, 171, 179, 255);
+    private Color nonSelect = new(178, 178, 178, 255);
+
     private void Start()
     {
         foreach (var button in buttons)
         {
-            button.targetGraphic.color = Color.white;
+            button.targetGraphic.color = nonSelect;
         }
         nowButton = buttons[0];
-        nowButton.targetGraphic.color = Color.green;
+        nowButton.targetGraphic.color = select;
     }
 
     public void OnTabChanged(Button nextButton)
     {
         if (nowButton == null) return;
 
-        nowButton.targetGraphic.color = Color.white;
+        nowButton.targetGraphic.color = nonSelect;
         nowButton = nextButton;
-        nowButton.targetGraphic.color = Color.green;
+        nowButton.targetGraphic.color = select;
     }
     public void OnEnable()
     {
         foreach (var button in buttons)
         {
-            button.targetGraphic.color = Color.white;
+            button.targetGraphic.color = nonSelect;
         }
         nowButton = buttons[0];
-        nowButton.targetGraphic.color = Color.green;
+        nowButton.targetGraphic.color = select;
     }
 }
