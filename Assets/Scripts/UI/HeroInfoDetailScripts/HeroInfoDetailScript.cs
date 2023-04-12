6b563b26b052919d47639bf730260d38baedb5e4
함민우 <108320084+minu1117@users.noreply.github.com>
2023-04-12 13:57:44 +0900

    히어로 정보창 UI 수정

--- Assets/Scripts/UI/HeroInfoDetailScripts/HeroInfoDetailScript.cs
+++ Assets/Scripts/UI/HeroInfoDetailScripts/HeroInfoDetailScript.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using System.Text;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class HeroInfoDetailScript : View
 {
@@ -9,6 +10,8 @@ public class HeroInfoDetailScript : View
     public TextMeshProUGUI levelInfoInLeftPanel;
     public Image typeInfoInLeftPanel;
     public TextMeshProUGUI statDetail;
+    public List<TextMeshProUGUI> statDetailNames;
+    public List<TextMeshProUGUI> statDetailTexts;
     public Toggle[] firstToggleTabs;
 
     public Slider expBar;
@@ -40,17 +43,29 @@ public class HeroInfoDetailScript : View
         gradeInfoInLeftPanel.text = $"{(CharacterGrade)data.grade}";
         levelInfoInLeftPanel.text = data.level.ToString();
         typeInfoInLeftPanel.sprite = gm.GetSpriteByAddress($"icon_type_{(CharacterJob)data.job}");
-        StringBuilder stringBuilder = new();
-        stringBuilder.Append($"히어로 명 : {gm.GetStringByTable(data.name)}\n");
-        stringBuilder.Append($"공격력 : {data.baseDamage:0.00}\n");
-        stringBuilder.Append($"방어력 : {data.baseDefense:0.00}\n");
-        stringBuilder.Append($"체력 : {data.healthPoint:0}\n");
-        stringBuilder.Append($"이동 속도 : {data.moveSpeed}\n");
-        stringBuilder.Append($"크리티컬 확률 : {data.critical * 100:0}%\n");
-        stringBuilder.Append($"크리티컬 배율 : {data.criticalDmg * 100:0}%\n");
-        //stringBuilder.Append($"명중률 : {data.accuracy * 100:0}%\n");
-        //stringBuilder.Append($"회피율 : {data.evasion * 100:0}%\n");
-        statDetail.text = stringBuilder.ToString();
+
+        List<string> infoNames = new List<string>
+        {
+            "히어로 명",
+            "공격력",
+            "방어력",
+            "체력",
+            "이동 속도",
+            "크리티컬 확률",
+            "크리티컬 배율"
+        };
+        List<string> infos = new List<string>
+        {
+            $"{gm.GetStringByTable(data.name)}",
+            $"{data.baseDamage:0.00}",
+            $"{data.baseDefense:0.00}",
+            $"{data.healthPoint:0}",
+            $"{data.moveSpeed}",
+            $"{data.critical * 100:0}",
+            $"{data.criticalDmg * 100:0}"
+        };
+        SetInfoText(statDetailNames, infoNames);
+        SetInfoText(statDetailTexts, infos);
         expBar.maxValue = gm.expRequirementTable[data.level];
         expBar.value = data.exp;
         expBar.GetComponentInChildren<TextMeshProUGUI>().text = $"EXP {expBar.value:0} / {expBar.maxValue:0}";
@@ -61,5 +76,13 @@ public class HeroInfoDetailScript : View
         skillInfo[1].text = $"{gm.GetStringByTable(cdb.passiveSkill.skillName)}\n{gm.GetStringByTable(cdb.passiveSkill.skillDescription)}";
         skillIcons[2].sprite = gm.GetSpriteByAddress($"{cdb.activeSkill.skillIcon}");
         skillInfo[2].text = $"{gm.GetStringByTable(cdb.activeSkill.skillName)}\n{gm.GetStringByTable(cdb.activeSkill.skillDescription)}";
+    }
+
+    private void SetInfoText(List<TextMeshProUGUI> infoTexts, List<string> buildTexts)
+    {
+        for (int i = 0; i < infoTexts.Count; i++)
+        {
+            infoTexts[i].text = buildTexts[i];
+        }
     }
 }
